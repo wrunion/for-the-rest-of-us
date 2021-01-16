0ccf7c66e6c71c7d932fe0f98c19b26fe032638a
wrunion <winterrunion@gmail.com>
2021-01-15 20:29:48 -0800

    Add cards to display individual resources.

--- src/components/NavBar/NavBarTop.jsx
+++ src/components/NavBar/NavBarTop.jsx
@@ -1,15 +1,12 @@
 import React, { Fragment } from 'react';
-// import NavBar from 'react-bootstrap/NavBar';
 import Button from 'react-bootstrap/Button';
 import { Navbar, Nav, NavItem, NavDropdown, Glyphicon } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-import Container from 'react-bootstrap/Container';
 
 const NavBarTop = () => {
   return (
     <Fragment>
-      {/* <Container> */}
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Nav className="mr-auto">
@@ -22,7 +19,6 @@ const NavBarTop = () => {
           <Button variant="outline-light">Search</Button>
         </Form>
       </Navbar>
-      {/* </Container> */}
     </Fragment>
   );
 }
---
+++ src/components/ResourceCard/ResourceCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+const cardTitleStyle = {
+  fontWeight: 'bold', 
+  fontFamily: 'Open Sans Condensed, sans-serif',
+  fontSize: '24px',
+  color: '#343a40',
+}
+
+const cardBodyStyle = {
+  color: '#606060',
+}
+
+const ResourceCard = props => {
+
+  return (
+    <Card style={{ width: '85%', margin: '1em auto'}}>
+      <Card.Body>
+        <Card.Title style={cardTitleStyle}>Free Code Camp</Card.Title>
+        <Card.Text style={cardBodyStyle}>
+        freeCodeCamp is a proven path to your first software developer job. More than 40,000 people have gotten developer jobs after completing this – including at big companies like Google and Microsoft.
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Categories: Free Resources, JavaScript, HTML, Beginner-Friendly</small>
+      </Card.Footer>
+  </Card>
+  );
+}
+
+export default ResourceCard;
